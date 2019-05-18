45d7ea5e2fa744e5f968aa24cde861470c93523c
Brandon L. Borrero <borrero509@outlook.com>
2019-05-18 16:11:09 -0700

    styling so far

--- src/pages/iframe/components/views/Forms/Form.js
+++ src/pages/iframe/components/views/Forms/Form.js
@@ -1,21 +1,18 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
-import {Store} from 'react-chrome-redux';
-import {withRouter} from 'react-router'
+import { Store } from "react-chrome-redux";
+import { withRouter } from "react-router";
 import "./Form.css";
 // import { deleteForm, addFormToUpdate } from "../../../actions/formActions.js";
 
 const store = new Store({
-    portName: 'COUNTING',
-  })
-
+  portName: "COUNTING"
+});
 
 class Form extends Component {
-
-
   deleteForm = (userId, formId) => {
     this.props.deleteForm(userId, formId);
-    this.props.history.push('/')
+    this.props.history.push("/");
   };
 
   addFormToUpdate = form => {
@@ -24,18 +21,43 @@ class Form extends Component {
   };
 
   render() {
+    // return (
+    // <FormWrapper>
+    //   <Name>{name}</Name>
+    //   <FieldCount>Field Count: {field_count}</FieldCount>
+    //   <div>
+    //     <Edit onClick={() => this.addFormToUpdate(this.props.form)}>
+    //       edit
+    //     </Edit>
+    //   </div>
+    //   <div>
+    //     <Delete
+    //       onClick={() => {
+    //         if (window.confirm("Are you sure you want to delete this form?"))
+    //           this.deleteForm(
+    //             this.props.form.user_id,
+    //             this.props.form.form_id
+    //           );
+    //       }}
+    //     >
+    //       delete
+    //     </Delete>
+    //   </div>
+    // </FormWrapper>
+
     const { id, name, field_count, user_id, form_id } = this.props.form;
     return (
-      <div className="Title" key={id}>
+      <div className="FormWrapper">
         <div className="Name">{name}</div>
-        <div className="Field">{field_count}</div>
-        <div className="Empty">
+        <div className="FieldCount">Field Count:{field_count}</div>
+        <div>
           <button onClick={() => this.addFormToUpdate(this.props.form)}>
             edit
           </button>
         </div>
-        <div className="Delete">
+        <div>
           <button
+            className="Delete"
             onClick={() => {
               if (window.confirm("Are you sure you want to delete this form?"))
                 this.deleteForm(
@@ -44,7 +66,7 @@ class Form extends Component {
                 );
             }}
           >
-            X
+            delete
           </button>
         </div>
       </div>
@@ -53,10 +75,47 @@ class Form extends Component {
 }
 
 const mapDispatchToProps = dispatch => {
-    return {
-        deleteForm: (userId, formId) => store.dispatch({type: "alias@DELETE_FORM", userId: userId, formId:formId}),
-        addFormToUpdate: (form) => store.dispatch({type: 'adias@ADD_FORM_TO_UPDATE', form: form})
-    }
-}
+  return {
+    deleteForm: (userId, formId) =>
+      store.dispatch({
+        type: "alias@DELETE_FORM",
+        userId: userId,
+        formId: formId
+      }),
+    addFormToUpdate: form =>
+      store.dispatch({ type: "adias@ADD_FORM_TO_UPDATE", form: form })
+  };
+};
 
-export default withRouter(connect(null, mapDispatchToProps)(Form));
+export default withRouter(
+  connect(
+    null,
+    mapDispatchToProps
+  )(Form)
+);
+
+// const { id, name, field_count, user_id, form_id } = this.props.form;
+// return (
+//   <div className="Title" key={id}>
+//     <div className="Name">{name}</div>
+//     <div className="Field">{field_count}</div>
+//     <div className="Empty">
+//       <button onClick={() => this.addFormToUpdate(this.props.form)}>
+//         edit
+//       </button>
+//     </div>
+//     <div className="Delete">
+//       <button
+//         onClick={() => {
+//           if (window.confirm("Are you sure you want to delete this form?"))
+//             this.deleteForm(
+//               this.props.form.user_id,
+//               this.props.form.form_id
+//             );
+//         }}
+//       >
+//         X
+//       </button>
+//     </div>
+//   </div>
+// );
