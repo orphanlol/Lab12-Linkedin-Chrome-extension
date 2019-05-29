43444dc9164fd755333591953f3d9477337064ff
Brandon L. Borrero <borrero509@outlook.com>
2019-05-29 14:28:01 -0700

    styled pop up box

--- src/pages/popup/Login.js
+++ src/pages/popup/Login.js
@@ -1,68 +1,72 @@
-import React, {Component} from 'react';
-import { connect } from 'react-redux';
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import "./popup.css";
 
 class Login extends Component {
-    constructor(props) {
-        super(props);
+  constructor(props) {
+    super(props);
+  }
+
+  componentDidMount() {
+    localStorage.setItem("firstName", this.props.firstName);
+    localStorage.setItem("lastName", this.props.lastName);
+    localStorage.setItem("id", this.props.id);
+  }
+
+  logout = () => {
+    localStorage.clear();
+  };
+
+  render() {
+    console.log(this.props);
+    let isLogin = null;
+
+    if (this.props.id === null) {
+      isLogin = (
+        <div className="popupBoxLogin">
+          <a
+            className="popupA"
+            href="https://linkedinextension.netlify.com"
+            target="_blank"
+          >
+            Login
+          </a>
+        </div>
+      );
+    } else {
+      isLogin = (
+        <div className="popupBoxLoggedIn">
+          <div>Welcome Back, {this.props.firstName}</div>
+          <div>
+            <a
+              className="popupA"
+              href="https://linkedinextension.netlify.com"
+              target="_blank"
+            >
+              Logout
+            </a>
+          </div>
+        </div>
+      );
     }
 
-    componentDidMount() {
-        localStorage.setItem('firstName', this.props.firstName)
-        localStorage.setItem('lastName', this.props.lastName)
-        localStorage.setItem('id', this.props.id)
-
-    }
-
-    logout = () => {
-        localStorage.clear()
-    }
-    
-
-    render() {
-
-
-        console.log(this.props)
-        let isLogin = null
-
-        if (this.props.id === null)
-        {
-            isLogin = (
-                <a href='https://linkedinextension.netlify.com' target='_blank'>Login In</a>
-            )
-        } else {
-            isLogin = (
-                <div>
-                    <div>
-                        Welcome Back {this.props.firstName}
-                    </div>
-                    <div>
-                        <a href='https://linkedinextension.netlify.com' target='_blank'>Logout</a>
-                    </div>
-                </div>
-            )
-        }
-            
-        return (
-            <div>
-                {isLogin}
-            </div>
-        )
-    }
-
+    return <div>{isLogin}</div>;
+  }
 }
 
 const mapStateToProps = state => {
-    return {
-        firstName: state.login.firstName,
-        lastName: state.login.lastName,
-        id: state.login.id
-    }
-}
+  return {
+    firstName: state.login.firstName,
+    lastName: state.login.lastName,
+    id: state.login.id
+  };
+};
 
 const mapDispatchToProps = dispatch => {
-    return {
-        
-    }
-}
+  return {};
+};
 
-export default connect(mapStateToProps, mapDispatchToProps)(Login)
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(Login);
